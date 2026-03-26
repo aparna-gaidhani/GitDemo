061c8bfc774bc38cf98d2bbb47abba06d1db0ef0
aparna-gaidhani <aparnag412@gmail.com>
2026-03-26 17:38:39 +0530

    branch check

---
+++ src/main/java/sample/maven/Test2.java
@@ -0,0 +1,12 @@
+package sample.maven;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("checking the branch");
+		// TODO Auto-generated method stub
+
+	}
+
+}
