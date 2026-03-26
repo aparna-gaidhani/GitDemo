1be960957b34db98b90f853cde28b1716af52b92
aparna-gaidhani <aparnag412@gmail.com>
2026-03-26 17:53:03 +0530

    test

--- src/main/java/sample/maven/Test2.java
+++ src/main/java/sample/maven/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("checking the branch");
+		System.out.println("sample testing for Github");
 		// TODO Auto-generated method stub
 
 	}
