ce8135acf523ec59d81acc86631a98d08a2efbc7
aparna-gaidhani <aparnag412@gmail.com>
2026-03-26 17:14:34 +0530

    test java is modified

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         
         System.out.println("Hii i am Aparna");
-    }
+        System.out.println(" Hii i amusing the git");
+        System.out.println("I am learing the command of git");
 
 }
--- src/main/java/sample/maven/Main.java
+++ src/main/java/sample/maven/Main.java
@@ -3,5 +3,6 @@ package sample.maven;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("it is Git x");
     }
 }
